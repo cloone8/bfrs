ba787d7e15996ee3a0bcf216af6b6192b2ac3bff
Wouter de Bruijn <wouter@wutru.io>
2024-03-20 23:54:08 +0100

    More formatting

--- cpr_bfvm/src/main.rs
+++ cpr_bfvm/src/main.rs
@@ -25,22 +25,35 @@ macro_rules! assign_allocator_and_build {
 macro_rules! assign_cellsize_and_build {
     ($args:expr, $builder:expr) => {
         match $args.cellsize {
-            cli_args::CellSize::U8 => assign_allocator_and_build!($args, $builder.with_cell_type::<u8>()),
-            cli_args::CellSize::U16 => assign_allocator_and_build!($args, $builder.with_cell_type::<u16>()),
-            cli_args::CellSize::U32 => assign_allocator_and_build!($args, $builder.with_cell_type::<u32>()),
-            cli_args::CellSize::U64 => assign_allocator_and_build!($args, $builder.with_cell_type::<u64>()),
-            cli_args::CellSize::U128 => assign_allocator_and_build!($args, $builder.with_cell_type::<u128>()),
+            cli_args::CellSize::U8 => {
+                assign_allocator_and_build!($args, $builder.with_cell_type::<u8>())
+            }
+            cli_args::CellSize::U16 => {
+                assign_allocator_and_build!($args, $builder.with_cell_type::<u16>())
+            }
+            cli_args::CellSize::U32 => {
+                assign_allocator_and_build!($args, $builder.with_cell_type::<u32>())
+            }
+            cli_args::CellSize::U64 => {
+                assign_allocator_and_build!($args, $builder.with_cell_type::<u64>())
+            }
+            cli_args::CellSize::U128 => {
+                assign_allocator_and_build!($args, $builder.with_cell_type::<u128>())
+            }
         }
-    }
+    };
 }
 
 macro_rules! assign_input_and_build {
     ($args:expr, $builder:expr) => {
         match $args.input {
             Some(input) => {
-                assign_cellsize_and_build!($args, $builder.with_reader(File::open(input).expect("Could not open input file")))
+                assign_cellsize_and_build!(
+                    $args,
+                    $builder.with_reader(File::open(input).expect("Could not open input file"))
+                )
             }
-            None => assign_cellsize_and_build!($args, $builder)
+            None => assign_cellsize_and_build!($args, $builder),
         }
     };
 }
@@ -49,28 +62,31 @@ macro_rules! assign_output_and_build {
     ($args:expr, $builder:expr) => {
         match $args.output {
             Some(output) => {
-                let output_file = File::options().create(true).truncate(true).write(true).open(output).expect("Could not open output file");
+                let output_file = File::options()
+                    .create(true)
+                    .truncate(true)
+                    .write(true)
+                    .open(output)
+                    .expect("Could not open output file");
                 assign_input_and_build!($args, $builder.with_writer(output_file))
             }
-            None => assign_input_and_build!($args, $builder)
+            None => assign_input_and_build!($args, $builder),
         }
     };
 }
 
 macro_rules! process_args_and_build_vm {
-    ($args:expr) => {
-        {
-            let vm_builder = VMBuilder::new().with_preallocated_cells($args.preallocated);
-            assign_output_and_build!($args, vm_builder)
-        }
-    };
+    ($args:expr) => {{
+        let vm_builder = VMBuilder::new().with_preallocated_cells($args.preallocated);
+        assign_output_and_build!($args, vm_builder)
+    }};
 }
 
 fn main() -> ExitCode {
     let args = CLIArgs::parse();
 
     simple_logger::init_with_level(args.verbosity.clone().into()).unwrap();
-    
+
     log::info!("Assigning VM options and building");
 
     let mut vm = process_args_and_build_vm!(args);
